9596efe7d4fb6d9892de47ead582f3a4f7208b38
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-27 23:53:03 +0700

    add add_answer_to_question

---
+++ app/routers/answers.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.functions import get_session
+
+questions = APIRouter()
+
+
+@questions.post("/questions/{id}/answers/")
+async def add_answer_to_question(id: int, session: AsyncSession = Depends(get_session)):
+    pass
