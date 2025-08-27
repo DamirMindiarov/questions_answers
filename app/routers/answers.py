04dce4418b0c56e222114a7c9e2c0379c429f047
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-27 23:55:41 +0700

    added get_answer_by_id del_answer_by_id

--- app/routers/answers.py
+++ app/routers/answers.py
@@ -11,3 +11,11 @@ async def add_answer_to_question(id: int, session: AsyncSession = Depends(get_se
     pass
 
 
+@answers.get("/answers/{id}")
+async def get_answer_by_id(id: int, session: AsyncSession = Depends(get_session)):
+    pass
+
+
+@answers.delete("/answers/{id}")
+async def del_answer_by_id(id: int, session: AsyncSession = Depends(get_session)):
+    pass
