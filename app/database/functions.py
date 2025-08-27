bb4046823c1722d0da5c4fbe521955caf2f72ed3
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-27 23:36:04 +0700

    added get_session() create_db()

---
+++ app/database/functions.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.db_questions_answers import session_async, engine_async
+from app.database.models import Base
+
+
+async def get_session():
+    async with session_async() as session:
+        yield session
+
+
+async def create_db():
+    async with engine_async.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
