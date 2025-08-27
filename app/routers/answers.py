21506ab2e96fcf97152351892e58120d72621621
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-27 23:54:01 +0700

    rename answers

--- app/routers/answers.py
+++ app/routers/answers.py
@@ -3,9 +3,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.functions import get_session
 
-questions = APIRouter()
+answers = APIRouter()
 
 
-@questions.post("/questions/{id}/answers/")
+@answers.post("/questions/{id}/answers/")
 async def add_answer_to_question(id: int, session: AsyncSession = Depends(get_session)):
     pass
+
+
