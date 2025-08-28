b3d2e4a607d1551ff50e1fc5291c3b89d2884ab6
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-28 16:21:36 +0700

    added pydentic into routes

--- app/routers/answers.py
+++ app/routers/answers.py
@@ -3,28 +3,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.functions import get_session
 from app.database.models import Answer
+from app.schemas.questions_answers import AnswerPydentic, AnswerText
 
 answers = APIRouter()
 
 
 @answers.post("/questions/{id}/answers/")
-async def add_answer_to_question(id: int, session: AsyncSession = Depends(get_session)):
-    answer = Answer(question_id=id, text="my answer")
+async def add_answer_to_question(id: int, answer: AnswerText, session: AsyncSession = Depends(get_session)) -> AnswerPydentic:
+    answer = Answer(question_id=id, text=answer.text)
     session.add(answer)
     await session.commit()
 
-    return answer
+    return AnswerPydentic(**answer.__dict__)
 
 
 @answers.get("/answers/{id}")
-async def get_answer_by_id(id: int, session: AsyncSession = Depends(get_session)):
+async def get_answer_by_id(id: int, session: AsyncSession = Depends(get_session)) -> AnswerPydentic:
     answer = await session.get(Answer, id)
 
-    return answer
+    return AnswerPydentic(**answer.__dict__)
 
 
 @answers.delete("/answers/{id}")
-async def del_answer_by_id(id: int, session: AsyncSession = Depends(get_session)):
+async def del_answer_by_id(id: int, session: AsyncSession = Depends(get_session)) -> str:
     answer = await session.get(Answer, id)
     await session.delete(answer)
     await session.commit()
