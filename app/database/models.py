588aefd75b9600f777177360234139c1305b302c
DamirMindiarov <akacuki1263547@gmail.com>
2025-08-28 14:39:01 +0700

    added table answers

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from uuid import uuid4
 
-
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -14,3 +16,16 @@ class Question(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+
+    answer = relationship("Answer", back_populates="question", cascade="all, delete-orphan")
+
+
+class Answer(Base):
+    __tablename__ = "answers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id", ondelete="CASCADE"))
+    user_id: Mapped[str] = mapped_column(UUID(as_uuid=True), unique=True, default=uuid4)
+    text: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+
+    question = relationship("Question", back_populates="answer")
